1a07327e524bcc3140c36ab2ff2874b23ec5a7b5
PradnyaDeshmukh <71390301+PradnyaDeshmukh@users.noreply.github.com>
2022-09-24 16:29:12 +0530

    48% to 47% accuracy
    Change in the RF model, n_estimators=1, max_depth=1

--- RF.py
+++ RF.py
@@ -14,8 +14,37 @@ X = df.drop(labels=["label"], axis=1)
 from sklearn.ensemble import RandomForestClassifier
 
 # model = LinearSVC(max_iter= 40000)
-model = RandomForestClassifier(n_estimators = 10000, random_state = 42)
+model = RandomForestClassifier(n_estimators = 1, max_depth=1)
 model.fit(X,Y)
+
+# rfc = RandomForestClassifier()
+# parameters = {
+#     "n_estimators":[1,2,3,4,5],
+#     "max_depth":[1,2,3,4,8,16,32,None]
+    
+# }
+
+# def display(results):
+#     print(f'Best parameters are: {results.best_params_}')
+#     print("\n")
+#     mean_score = results.cv_results_['mean_test_score']
+#     std_score = results.cv_results_['std_test_score']
+#     params = results.cv_results_['params']
+#     for mean,std,params in zip(mean_score,std_score,params):
+#         print(f'{round(mean,3)} + or -{round(std,3)} for the {params}')
+
+# from sklearn.model_selection import GridSearchCV
+# cv = GridSearchCV(rfc,parameters,cv=5)
+# cv.fit(X,Y)
+# display(cv)
+
+
+
+
+
+
+
+
 
 df_test = pd.read_csv("testSet.csv")
 
