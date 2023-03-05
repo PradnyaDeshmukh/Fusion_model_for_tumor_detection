0cc2addf71b813e5c9a44722f4a99d7566c57345
PradnyaDeshmukh <71390301+PradnyaDeshmukh@users.noreply.github.com>
2023-03-05 10:33:10 +0530

    final commit

---
+++ DL_mdule/VGG16.py
@@ -0,0 +1,9 @@
+# import tensorflow
+from tensorflow import keras
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+
+model = VGG16(weights='imagenet', include_top=False)
+model.summary()
--- angleAvgTest.py
+++ angleAvgTest.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import os
 import re
-from conf import TEST_DIR
+from ..conf import TEST_DIR
 from skimage.color import rgb2gray
 from skimage.feature import graycomatrix, graycoprops
 
